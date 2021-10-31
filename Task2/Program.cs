f65e12bb152e431955d57351be287e892ec78697
Zevakinaite <zevakinaite@gmail.com>
2021-10-31 18:00:40 +0200

    Task2 commit

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string skaiciusPirmas;
+            string skaiciusAntras;
+
+            //Enter first number:
+            Console.WriteLine("Enter first number");
+            //2
+            skaiciusPirmas = Console.ReadLine();
+            // Enter second number:
+            Console.WriteLine("Enter second number");
+            // 4
+            skaiciusAntras = Console.ReadLine();
+            //Calculated sum is: 6
+            int result = int.Parse(skaiciusPirmas) + int.Parse(skaiciusAntras);
+            Console.WriteLine($"Sum is: {result}");
+            // “result” is variable name;
+            // $ nurodo, kad skliaustuose gali buti kintamasis - siuo atveju result
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
